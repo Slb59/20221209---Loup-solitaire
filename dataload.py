e82c4089926cc97bde618af34c4c8b4e299a221f
Slb59 <sylvie.bricout2@laposte.net>
2022-12-09 21:19:25 +0100

    Read csv file

--- dataload.py
+++ dataload.py
@@ -1,6 +1,13 @@
-import openpyxl
+from csv import reader
 
 class DataLoad:
     def __init__(self):
-        path = "C:\\Users\\brico\\Documents\\8_Informatique\\2_Loup solitaire\\Tome1\\loup.ods"
+        path = "C:\\Users\\brico\\Documents\\8_Informatique\\2_Loup solitaire\\Tome1\\loup.csv"
+        self.rowList = []
+        with open(path, 'r') as csvObj:
+            csv_reader = reader(csvObj, delimiter=';')
+            self.rowList = list(csv_reader)
+
+
+
 
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 from dataload import DataLoad
 
 data = DataLoad()
+
+print(data.rowList[1][0])
